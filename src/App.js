9220a8efaa17d4671ccfdd6e7b2b55b7e26344a9
Abhinav <abhinavnegi842@gmail.com>
2021-06-12 01:39:27 +0530

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div className="App">
       <h1>Lets Build A Covid 19 Tracker!!!</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
